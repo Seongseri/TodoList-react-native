c5f1604ddaa2442ac06dc0ef2a33fd5744d5b349
Seongseri <tjdtpfl@gmail.com>
2023-01-10 00:05:53 +0900

    헤더 완료

---
+++ App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import {StyleSheet} from 'react-native';
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
+import DateHead from './components/DateHead';
+
+function App() {
+  const today = new Date();
+
+  return (
+      <SafeAreaProvider>
+        <SafeAreaView edges={['bottom']}>
+          <DateHead date={today} />
+        </SafeAreaView>
+      </SafeAreaProvider>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default App;
---
+++ components/DateHead.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { View, Text, StyleSheet, StatusBar } from 'react-native';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
+
+function DateHead({date}) {
+    const year = date.getFullYear();
+    const month = date.getMonth() + 1;
+    const day = date.getDate();
+    
+    const {top} = useSafeAreaInsets();
+
+    const formatted = `${year}년 ${month}월 ${day}일`;
+
+    return (
+        <>
+        <View style={[styles.StatusBarPlaceholder, {height: top}]} />
+        <StatusBar backgroundColor={'#000049'}/>
+            <View style={styles.block}>
+                <Text style={styles.dateText}>{formatted}</Text>
+            </View>
+        </>
+    );
+}
+
+const styles = StyleSheet.create({
+    StatusBarPlaceholder: {
+        backgroundColor: '#000049',
+    },
+    block: {
+        padding: 16,
+        backgroundColor: '#000049',
+    },
+    dateText: {
+        fontSize: 24,
+        color: 'white',
+    }
+});
+
+export default DateHead;
